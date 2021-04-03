ed8d75f0090efa35ebd41dc77edf3d5f6dd8f635
jrflorentine <53338319+jrflorentine@users.noreply.github.com>
2021-04-03 23:14:37 +0100

    Update main.blocks, main.ts

--- main.ts
+++ main.ts
@@ -1,18 +1,19 @@
 pins.setPull(DigitalPin.P0, PinPullMode.PullUp)
-servos.P1.setAngle(20)
+servos.P1.setAngle(30)
 basic.forever(function () {
     while (input.buttonIsPressed(Button.A)) {
-        servos.P1.setAngle(160)
+        servos.P1.setAngle(150)
     }
     if (pins.digitalReadPin(DigitalPin.P0) == 0) {
         for (let index = 0; index < 2; index++) {
-            servos.P1.setAngle(160)
-            basic.pause(500)
-            servos.P1.setAngle(20)
+            servos.P1.setAngle(150)
+            basic.pause(400)
+            servos.P1.setAngle(30)
+            basic.pause(400)
         }
     }
     while (pins.digitalReadPin(DigitalPin.P0) == 0) {
-        servos.P1.setAngle(160)
+        servos.P1.setAngle(150)
     }
-    servos.P1.setAngle(20)
+    servos.P1.setAngle(30)
 })
